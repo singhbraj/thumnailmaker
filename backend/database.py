dfbf9d71f84c5994770e48da7f151b5bb3a7631c
singhbraj <braj333singh@gmail.com>
2026-06-01 01:03:17 +0530

    add database connection logic

---
+++ backend/database.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, create_engine, Session
+from config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread:False"})
+
+def create_table():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
